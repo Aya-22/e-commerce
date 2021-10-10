61efd296cdd0082fa939dffc83cf2af8b75b1507
Jennifer Ayala <j.ayala0695@gmail.com>
2021-10-10 16:03:32 -0700

    updated index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+ through: {
+   model: ProductTag,
+   unique: false
+ },
+ as: 'priced_item'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tagged_product'
+})
 
 module.exports = {
   Product,
